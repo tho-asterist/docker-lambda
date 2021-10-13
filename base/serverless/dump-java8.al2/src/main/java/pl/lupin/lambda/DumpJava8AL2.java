0b79f5bc57932cacbacb1f7e303fe34852ccb578
Maciej Wilczyński <maciej.wilczynski@nordcloud.com>
2021-10-13 12:57:25 +0200

    Add java8.al2 dumping

--- base/serverless/dump-java8.al2/src/main/java/pl/lupin/lambda/DumpJava8AL2.java
+++ base/serverless/dump-java8.al2/src/main/java/pl/lupin/lambda/DumpJava8AL2.java
@@ -1,4 +1,4 @@
-package org.lambci.lambda;
+package pl.lupin.lambda;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,21 +9,18 @@ import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Scanner;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.lambda.runtime.Context;
 
-public class DumpJava8 implements RequestHandler<Object, PutObjectResult> {
-
-    @Override
-    public PutObjectResult handleRequest(Object input, Context context) {
-        String filename = "java8.al2.tgz";
-        String cmd = "tar -cpzf /tmp/" + filename + " --numeric-owner --ignore-failed-read /var/runtime /var/lang";
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion("us-east-1").build();
+public class DumpJava8AL2 {
+    public PutObjectResult handle(Object input, Context context, String arch) {
+        String filename = "java8.al2-" + arch + ".tgz";
+        String cmd = "touch /tmp/" + filename + " && tar -cpzf /tmp/" + filename + " --numeric-owner --ignore-failed-read /var/runtime /var/lang";
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion("eu-central-1").build();
 
         System.out.println(ManagementFactory.getRuntimeMXBean().getInputArguments().toString());
         System.out.println(System.getProperty("sun.java.command"));
@@ -60,7 +57,7 @@ public class DumpJava8 implements RequestHandler<Object, PutObjectResult> {
 
             System.out.println("Zipping done! Uploading...");
 
-            return s3client.putObject(new PutObjectRequest("lambci", "fs/" + filename, new File("/tmp/" + filename))
+            return s3client.putObject(new PutObjectRequest("docker-lambda", "fs/" + filename, new File("/tmp/" + filename))
                     .withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (Exception e) {
             throw new RuntimeException(e);
---
+++ base/serverless/dump-java8.al2/src/main/java/pl/lupin/lambda/DumpJava8AL2_arm64.java
@@ -0,0 +1,15 @@
+package pl.lupin.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.model.PutObjectResult;
+
+import pl.lupin.lambda.DumpJava8AL2;
+
+public class DumpJava8AL2_arm64 implements RequestHandler<Object, PutObjectResult> {
+    @Override
+    public PutObjectResult handleRequest(Object input, Context context) {
+        DumpJava8AL2 dump = new DumpJava8AL2();
+        return dump.handle(input, context, "arm64");
+    }
+}
