dcdbfe74d1ff450f249a0e6c0efb834b5ce4b80b
Maciej Wilczyński <maciej.wilczynski@nordcloud.com>
2021-10-13 18:18:29 +0200

    Update publish scripts

--- base/base-2-x86_64/publish.sh
+++ base/base-2-x86_64/publish.sh
@@ -2,7 +2,12 @@
 
 export PUBLISH_DATE=$(date "+%Y%m%d%H%M")
 export BASE_IMAGE="mlupin/lambda-base-2:x86_64"
+export CC_IMAGE="mlupin/docker-lambda:base-x86_64"
 
 docker tag ${BASE_IMAGE} ${BASE_IMAGE}-${PUBLISH_DATE}
+docker tag ${BASE_IMAGE} ${CC_IMAGE}
+docker tag ${BASE_IMAGE} ${CC_IMAGE}-${PUBLISH_DATE}
 docker push ${BASE_IMAGE}
 docker push ${BASE_IMAGE}-${PUBLISH_DATE}
+docker push ${BASE_IMAGE} ${CC_IMAGE}
+docker push ${BASE_IMAGE} ${CC_IMAGE}-${PUBLISH_DATE}
