fb04fec7ed0b8605908bcde2d4b0141491e7c4ec
Maciej Wilczyński <maciej.wilczynski@nordcloud.com>
2021-10-14 12:30:25 +0200

    Fix find args

--- runtimes/get-all-runtimes.sh
+++ runtimes/get-all-runtimes.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 export DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
-find $DIR -type d -depth 1 | rev | cut -d'/' -f1 | cut -d'-' -f2- | rev | sort -u
+find $DIR -depth 1 -type d | rev | cut -d'/' -f1 | cut -d'-' -f2- | rev | sort -u
