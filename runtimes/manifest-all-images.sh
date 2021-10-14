ff0c2d794107cfda24244c9165f127da27670802
Maciej Wilczyński <maciej.wilczynski@nordcloud.com>
2021-10-14 22:06:07 +0200

    Fix manifest creation

--- runtimes/manifest-all-images.sh
+++ runtimes/manifest-all-images.sh
@@ -6,10 +6,12 @@ export DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 for runtime in $($DIR/get-all-runtimes.sh); do
     image="mlupin/docker-lambda:${runtime}"
 
+    docker manifest rm $image || true
     docker manifest create $image \
         --amend ${image}-x86_64 \
         --amend ${image}-arm64
 
+    docker manifest rm ${image}-build || true
     docker manifest create ${image}-build \
         --amend ${image}-build-x86_64 \
         --amend ${image}-build-arm64
