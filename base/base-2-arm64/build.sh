024d9875942d6ab8b7f539bf989023368b6b128a
Maciej Wilczyński <maciej@lupine.software>
2023-08-10 15:16:23 +0200

    Fix base cross-platform builds

--- base/base-2-arm64/build.sh
+++ base/base-2-arm64/build.sh
@@ -4,6 +4,6 @@ curl -O https://docker-lambda.s3.amazonaws.com/fs/base-2-arm64.tgz
 
 set -euxo pipefail
 
-docker build --progress plain --no-cache --squash -t mlupin/lambda-base-2:arm64 .
+docker build --progress plain --no-cache --platform linux/arm64 --squash -t mlupin/lambda-base-2:arm64 .
 
 rm ./base-2-arm64.tgz
--- base/build-2-x86_64/build.sh
+++ base/build-2-x86_64/build.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-docker build --progress plain --no-cache --squash -t mlupin/lambda-base-2:build-x86_64 .
+docker build --progress plain --platform linux/amd64 --no-cache --squash -t mlupin/lambda-base-2:build-x86_64 .
